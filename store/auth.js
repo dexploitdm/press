20b8ac09c23767bd9b2fd636bbdcd4a68b5c253f
Дмитрий Паршаков <dexploitdm@yandex.ru>
2019-06-15 21:49:30 +0500

    add page users

--- store/auth.js
+++ store/auth.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-    token: null
+    token: true
 })
 
 export const mutations = {
@@ -29,6 +29,14 @@ export const actions = {
     },
     logout({commit}){
         commit('clearToken')
+    },
+    async createUser({commit}, formData){
+        try {
+            console.log(formData)
+        }
+        catch (e) {
+
+        }
     }
 }
 
